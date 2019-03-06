630d820ddfd95a54f431cb0265c217348c640663
lcx <liu.changxin@seu.edu.cn>
2019-03-06 23:16:38 +0800

    add Demo field

--- src/Demo.java
+++ src/Demo.java
@@ -1,10 +1,14 @@
 public class Demo {
   private String id;
   private String name;
+  private Integer age;
+  private String school;
 
-  public Demo(String id, String name) {
+  public Demo(String id, String name, Integer age, String school) {
     this.id = id;
     this.name = name;
+    this.age = age;
+    this.school = school;
   }
 
   @Override
@@ -12,6 +16,8 @@ public class Demo {
     return "Demo{" +
       "id='" + id + '\'' +
       ", name='" + name + '\'' +
+      ", age=" + age +
+      ", school='" + school + '\'' +
       '}';
   }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
   public static void main(String[] args) {
-    Demo demo = new Demo("id", "name");
+    Demo demo = new Demo("id", "name", 23, "seu");
     System.out.println(demo.toString());
     System.out.println("merge test");
   }
